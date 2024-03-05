79515c16f56deede9a6fc8426643858a104447a6
seroven <sebastianrodriguezventura@gmail.com>
2024-03-05 12:44:33 -0500

    fix 404 page

--- resources/js/modules/error/404.vue
+++ resources/js/modules/error/404.vue
@@ -2,8 +2,8 @@
     <div class="flex align-items-center flex-column">
         <span class="font-bold text-4xl">404</span>
         <span class="text-md text-center">La página a la que intentas acceder <b>no existe</b></span>
-        <span class="text-md">Para regresar al panel haz clic
-            <router-link class="" :to="{name: 'Home'}">aquí</router-link>
+        <span class="text-md">Para regresar al panel de estándares haz clic
+            <router-link class="" :to="{name: 'Standards'}">aquí</router-link>
         </span>
     </div>
 </template>
